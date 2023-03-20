97de195e5a81a7b5965141f2d4be8b190641e3c9
Abdi Halake <abdhata@outlook.com>
2023-03-20 19:05:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Declare a global variable called customerName
+var customerName = 'bob';
+
+// Write a function to access and uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Write a function to declare and assign a global variable called bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Write a function to overwrite the value of the global variable bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Declare a global constant called leastFavoriteCustomer
+const leastFavoriteCustomer = 'John';
+
+// Write a function to attempt changing the value of the global constant leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Jane'; // This will result in an error because you cannot reassign a constant
+}
+
